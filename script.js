a619cfee7a93c5b462efc53669635f3682454ea3
StealthSov <50644368+StealthSov@users.noreply.github.com>
2020-10-16 13:11:41 -0400

    Added Key Positions

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let chars = [];
+
+//Customizable Parameters
+let keyWidth = 108;
+let keyPadding = 10;
+
+
+
 function setup(){
   createCanvas(windowWidth,windowHeight);
   background(0);
@@ -16,6 +23,54 @@ function draw(){
   }
 }
 
+let keys = {
+  "1":1,
+  "q":1.3,
+  "a":1.5,
+  "z":1.7,
+  "2":2,
+  "w":2.3,
+  "s":2.5,
+  "x":2.9,
+  "3":3,
+  "e":3.3,
+  "d":3.5,
+  "c":3.7,
+  "4":4,
+  "r":4.3,
+  "f":4.5,
+  "v":4.9,
+  "5":5,
+  "t":5.3,
+  "g":5.5,
+  "b":5.9,
+  "6":6,
+  "y":6.3,
+  "h":6.5,
+  "n":6.9,
+  "7":7,
+  "u":7.3,
+  "j":7.5,
+  "m":8,
+  "8":8,
+  "i":8.3,
+  "k":8.5,
+  ",":9,
+  "9":9,
+  "o":9.3,
+  "l":9.5,
+  ".":10,
+  "0":10,
+  "p":10.3,
+  ";":10.5,
+  "/":11,
+  "-":11,
+  "[":11.3,
+  "'":11.5,
+  "=":12,
+  "]":12.3,
+}
+
 function keyPressed(){
   if(key.length == 1){
     chars.push(new CharObject(key))
@@ -26,7 +81,8 @@ class CharObject {
   constructor(character) {
     this.char = character;
     this.velocity = createVector(random(-4, 4), random(-12, -7));
-    this.position = createVector(random((windowWidth/2)-100, (windowWidth/2)+100), windowHeight);
+    let x = keyPadding + keys[this.char]*keyWidth;
+    this.position = createVector(x, windowHeight);
   }
 
   update(){
